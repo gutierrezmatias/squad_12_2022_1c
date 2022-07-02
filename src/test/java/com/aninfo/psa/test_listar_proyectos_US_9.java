338c83331f43e383821d240b6e916f7e996cdfde
Alexis094 <ramos.ale094@gmail.com>
2022-07-02 17:46:08 -0300

    se corrige falla en test us-9

--- src/test/java/com/aninfo/psa/test_listar_proyectos_US_9.java
+++ src/test/java/com/aninfo/psa/test_listar_proyectos_US_9.java
@@ -4,6 +4,7 @@ import com.aninfo.psa.Services.ProyectoService;
 import com.aninfo.psa.Services.TareaService;
 import com.aninfo.psa.modelo.Proyecto;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,19 +25,34 @@ public class test_listar_proyectos_US_9 {
 
     List<Proyecto> lista_proyectos;
 
-    @Transactional
-    @And("una lista de proyectos creados: {string}, {string}, {string}")
-    public void unaListaDeProyectosCreados(String arg0, String arg1, String arg2) {
-        proyectoService.crearProyecto(new Proyecto(arg0));
-        proyectoService.crearProyecto(new Proyecto(arg1));
-        proyectoService.crearProyecto(new Proyecto(arg2));
+    private Proyecto proyecto1;
+    private Proyecto proyecto2;
+    private Proyecto proyecto3;
 
-        proyectoService.buscarPorID(1L).get().dar_baja();
+    @Transactional
+    @Given("una lista de proyectos creados: {string}, {string}, {string}")
+    public void unaListaDeProyectosCreados(String arg0, String arg1, String arg2) {
+        proyecto1 = new Proyecto(arg0);
+        proyecto2 = new Proyecto(arg1);
+        proyecto3 = new Proyecto(arg2);
+
+        proyectoService.crearProyecto(proyecto1);
+        proyectoService.crearProyecto(proyecto2);
+        proyectoService.crearProyecto(proyecto3);
+
+        proyectoService.buscarPorID(proyecto1.getid()).get().dar_baja();
+
+        assertEquals( proyectoService.buscarPorID(proyecto1.getid()).get().getEstado(), "Interrumpido");
     }
 
     @Transactional
     @When("solicito los proyectos creados")
     public void solicitoLosProyectosCreados() {
+
+        assertEquals(proyectoService.buscarPorID(proyecto1.getid()).get().getNombre(), proyecto1.getNombre());
+        assertEquals(proyectoService.buscarPorID(proyecto2.getid()).get().getNombre(), proyecto2.getNombre());
+        assertEquals(proyectoService.buscarPorID(proyecto3.getid()).get().getNombre(), proyecto3.getNombre());
+
         lista_proyectos = proyectoService.obtenerProyectos();
     }
 
@@ -51,10 +67,26 @@ public class test_listar_proyectos_US_9 {
     @Transactional
     @And("primero apareceran <{string}> y <{string}>")
     public void primeroApareceranY(String arg0, String arg1) {
-        assertEquals(arg0,lista_proyectos.get(1).getNombre());
-        assertEquals(arg1,lista_proyectos.get(0).getNombre());
+
+       int i = 0;
+       int indice1 = 0;
+       int indice2 = 0;
+
+       while(i < lista_proyectos.size()){
+            if(lista_proyectos.get(i).getNombre().equals(arg0)){
+                indice1 = i;
+            } else if(lista_proyectos.get(i).getNombre().equals(arg1)){
+                indice2 = i;
+            }
+            i++;
+       }
+
+       //el proyecto con estado interrumpido se muesta ultimo
+       assert(indice1 > indice2);
     }
 
+
+    //escenario 2------------------
     @Transactional
     @When("solicite buscar los proyectos con {string}")
     public void soliciteBuscarLosProyectosCon(String arg0) {
@@ -64,7 +96,11 @@ public class test_listar_proyectos_US_9 {
     @Transactional
     @Then("se mostraran los proyectos: {string}, {string}")
     public void seMostraranLosProyectos(String arg0, String arg1) {
+
+        assertEquals(lista_proyectos.size(), 2);
+
         assertEquals(lista_proyectos.get(0).getNombre(), arg0);
         assertEquals(lista_proyectos.get(1).getNombre(), arg1);
+
     }
 }
--- src/test/resources/listar_proyectos(US-9).feature
+++ src/test/resources/listar_proyectos(US-9).feature
@@ -1,14 +1,12 @@
 Feature: Listar proyectos {US-9}
 
   Scenario: Existe una lista de proyectos y uno de ellos no esta activo
-    Given que soy un empleado
-    And  una lista de proyectos creados: "PSA-001", "PSA-002", "PSA-003"
+    Given una lista de proyectos creados: "PSA-001", "PSA-002", "PSA-003"
     When solicito los proyectos creados
     Then se mostrarán los proyectos: "PSA-001", "PSA-002", "PSA-003"
     And primero apareceran <"PSA-003"> y <"PSA-002">
 
   Scenario: Exista una lista de proyectos y se buscan aquellos que contengan cierto string
-    Given que soy un empleado
-    And una lista de proyectos creados: "Un proyecto de google", "Proyecto para organizaciones", "Product para google"
-    When solicite buscar los proyectos con "proy"
-    Then se mostraran los proyectos: "Un proyecto de google", "Proyecto para organizaciones"
+    Given una lista de proyectos creados: "google project", "Orgs project", "google product"
+    When solicite buscar los proyectos con "proj"
+    Then se mostraran los proyectos: "google project", "Orgs project"
